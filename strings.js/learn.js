83368b917190a92f7fd2fb0af15998389f341b88
ABHIJEtsingh <94282804+ABHIJEtsingh@users.noreply.github.com>
2023-09-24 18:28:44 +0000

    strings

---
+++ strings.js/learn.js
@@ -0,0 +1,10 @@
+const name = 'VishalSingh' ;
+const Branch = "Cse";
+const Year = 3;
+    
+ //mordern technique of concatination
+console.log(`My self ${name} and From ${Branch} in ${Year}year`);
+const gameName = new string("Abhijeet")
+
+//Using Objects of the strings
+const Name = new string('Prithvi')
