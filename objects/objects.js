f3f7938f76e201a2c906c29c42aff7648269ba35
ABHIJEtsingh <94282804+ABHIJEtsingh@users.noreply.github.com>
2023-09-28 09:08:37 +0000

    types of objects

---
+++ objects/objects.js
@@ -0,0 +1,10 @@
+// singleton - Literal are not singleton but 
+
+//object literals 
+
+const jsUser = {
+    name:'abhijeet',
+    practicehHrs : 3,
+    projects : ["calculator","todolist","daynigth"]
+}
+console.log(jsUser)
